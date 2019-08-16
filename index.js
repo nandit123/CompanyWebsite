96bddfd97cc139d2a25a5e9a20872a0ebe1ead83
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2019-08-17 01:03:57 +0530

    csp for companies.html

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 app.use(bodyParser.json())  
 
+app.use(function(req, res, next) {
+    res.header("Content-Security-Policy", "img-src 'self'");
+    next();
+});
+
 // caching disabled for every route
 app.use(function(req, res, next) {
     res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
