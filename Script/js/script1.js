f6828066f1670357c4a463dc61c7ce51aff2aaf6
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2019-08-19 16:07:46 +0530

    changed 13.232.225.137:3000 to buytrue.in

--- Script/js/script1.js
+++ Script/js/script1.js
@@ -35,7 +35,7 @@ $("#loginForm").on('submit',function(event) {
                 dataString = data;
                 $.ajax({
                     type: "POST",
-                    url: "http://13.232.225.137:3000/sessionStart",
+                    url: "http://buytrue.in/sessionStart",
                     data: dataString
                 }).always(function(data){
                     window.location.href = "dashboard";
