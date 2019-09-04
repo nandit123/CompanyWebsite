408aa687a665a1bf03c93dbe88949714f28f2f99
EC2 Default User <ec2-user@ip-172-31-47-244.ap-south-1.compute.internal>
2019-09-04 11:39:46 +0000

    added pdfHtml5 button

--- Script/js/demo/datatables-demo.js
+++ Script/js/demo/datatables-demo.js
@@ -5,12 +5,19 @@ $(document).ready(function() {
         searching: true,
         dom: 'Bfrtip',
         buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
+            'copy', 'csv', 'excel', 'pdf', 'print',
+	    {
+		extend: 'pdfHtml5',
+		exportOptions: {
+		    stripHtml: false
+		}
+	    }
         ],
         "columnDefs": [
             {
+		"targets": 4,
                 "data": "QR Code",
-                "render": function(data, type, row) {
+                "render": function(data, type, row, meta) {
                     return '<img src="'+data+'" />';
                 }            
             }
